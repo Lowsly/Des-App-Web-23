30a81945f0f78d056f6afa29ee4ec7621b64ea42
Miranda Díaz <210050@mail.unacar.mx>
2023-03-09 00:47:34 -0600

    Ejercicio2.js

--- 03-Logica/Ejercicio2.js
+++ 03-Logica/Ejercicio2.js
@@ -1,6 +1,6 @@
 function conversion(){
 
-    let far = parseFloat (prompt)("Ingresa la temperatura en grados Farenheit")
+    let far = parseFloat (prompt)("Ingresa la temperatura en grados Farenheit: ")
     let cent= (far-32) ÷ 1.8;
     let resultado = conversion();
 }
