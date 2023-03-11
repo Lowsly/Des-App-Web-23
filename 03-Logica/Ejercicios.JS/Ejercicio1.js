4931aa4e11b8c33034a450f656f52a5562f420a2
Miranda Díaz <210050@mail.unacar.mx>
2023-03-11 14:44:41 -0600

    Cambios

--- 03-Logica/Ejercicios.JS/Ejercicio1.js
+++ 03-Logica/Ejercicios.JS/Ejercicio1.js
@@ -1,13 +1,15 @@
-function verificarTarea() {
+function verTarea() {
     let tarea = prompt("¿Hiciste la tarea?");
   
     if (tarea.toLowerCase() === "si") {
       document.write("¡Bien hecho!");
-    } else if (tarea.toLowerCase() === "no") {
+    } 
+    else if (tarea.toLowerCase() === "no") {
       document.write("No te rindas, ¡empieza ya mismo!");
-    } else {
-      document.write("Por favor, ingresa un valor válido ('si' o 'no').");
+    } 
+    else {
     }
+    document.write("Por favor, ingresa un valor válido >>'si' o 'no'<<");
   }
-  verificarTarea();
+  verTarea();
   
